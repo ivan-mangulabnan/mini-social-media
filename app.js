5fdb42c722a863601408c4c14ccd45606e12bdfb
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-11-21 13:08:18 +0800

    set up configs

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import router from './routes/routes.js';
+import pool from './db/pool.js';
+import connectPgSimple from 'connect-pg-simple';
 
 const app = express();
+const PgSession = connectPgSimple(session);
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filepath);
+const views = path.join(__dirname, 'views');
+const assetsPath = path.join(__dirname, 'public');
+const sessionOptions = {
+  store: new PgSession({ pool }),
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false
+};
+
+app.set('views', views);
+app.set('view engine', 'ejs');
+
+app.use(session(sessionOptions));
+app.use(passport.session());
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
 
 app.listen(process.env.PORT, (err) => {
   if (err) return console.log(err);
