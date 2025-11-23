84cca3d459edb1dcb4102b96817d8cad60c1f775
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-11-23 19:49:47 +0800

    add anonimity if not a member

---
+++ db/queries/post.js
@@ -0,0 +1,12 @@
+import pool from "../pool.js";
+
+const createPost = async (id, message) => {
+  await pool.query('INSERT INTO posts (owner_id, message) VALUES ($1, $2)', [id, message]);
+}
+
+const getPosts = async () => {
+  const { rows } = await pool.query('SELECT p.*, a.fname, a.lname FROM posts AS p INNER JOIN accounts AS a ON p.owner_id = a.id ORDER BY created_at DESC');
+  return rows;
+}
+
+export default { createPost, getPosts };
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,51 @@
 import { Router } from "express";
 import loginRoute from "./login.js";
 import signUpRoute from "./sign-up.js";
+import { body, matchedData, validationResult } from "express-validator";
+import db from '../db/queries/post.js';
 
 const router = Router();
 
 router.use('/login', loginRoute);
 router.use('/sign-up', signUpRoute);
-router.get('/', (req, res) => {
+
+router.get('/', (req, res, next) => {
   if (!req.isAuthenticated()) return res.redirect('/login');
-  res.render('index'); 
+  next();
+}, async (req, res, next) => {
+  try {
+    res.locals.posts = await db.getPosts();
+  } catch (err) {
+    return next(err);
+  }
+
+  res.render('index');
 });
+
+router.post('/', (req, res, next) => {
+ if (!req.isAuthenticated()) return res.redirect('/login');
+ next();
+}, [
+  body('id').trim().notEmpty().withMessage('id should not be empty').isInt({ min: 1 }).withMessage('id should be a number').custom((value, { req }) => {
+    if (req.user.id !== parseInt(value)) throw new Error('id do not match');
+    return true;
+  }),
+  body('message').trim().notEmpty().withMessage('message should not be empty')
+], async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return next(new Error(JSON.stringify(errors.array())));
+  const { id, message } = matchedData(req, { locations: ['body'] });
+  try {
+    await db.createPost(id, message);
+  } catch (err) {
+    return next(err)
+  }
+
+  res.redirect('/');
+})
+
+router.use((err, req, res, next) => {
+  return res.send('Something went wrong in index route');
+})
 
 export default router;
